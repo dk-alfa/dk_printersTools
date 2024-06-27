baf6cd50adcb11cfcaf8365da4a37f91bf8e2a04
dkorablev <dkorablev@toyota-ufa.ru>
2024-06-27 17:05:45 +0500

    Добавлено: получение значений counters

--- controller/device/printer.py
+++ controller/device/printer.py
@@ -20,33 +20,49 @@ class Printers:
     def __get_priners_list_test(self):
         printers_list = []
         printers_list.append({'ip_address': '172.16.1.226', 'type': '1','login':'Admin','password':'Admin'})
-        #printers_list.append({'ip_address': '172.16.0.232', 'type': '1','login':'Admin','password':'Admin'})
+        # printers_list.append({'ip_address': '172.16.0.232', 'type': '1','login':'Admin','password':'Admin'})
         # printers_list.append({'ip_address': '172.16.0.223',   'type': '1','login':'Admin','password':'Admin'})
         return printers_list
 
 class Printer:
     def get_counters(self,printer_info):
+        def login(driver):
+            frame = driver.find_element(By.NAME, 'wlmframe')
+            driver.switch_to.frame(frame)
+            driver.find_element(By.ID, 'arg01_UserName').send_keys(printer_info["login"])
+            driver.find_element(By.ID, 'arg02_Password').send_keys(printer_info["password"])
+            driver.find_element(By.NAME, 'Login').click()
+        def exit_from_standby_mode(driver):
+            frame = driver.find_element(By.NAME, 'main')
+            driver.switch_to.frame(frame)
+            driver.find_element(By.NAME,'submit001').click()
+
         url = f'http://{printer_info["ip_address"]}'
         driver = wd.Chrome()
         driver.implicitly_wait(10)
         driver.get(url)
 
-        ##Иногда принтер находится в режиме экономии энергии, в этом случае проверяем этот факт, и выводим его из этого режима
-        # frame = driver.find_element(By.TAG_NAME, 'frame')
-        # driver.switch_to.frame(frame)
-        # driver.find_element(By.NAME,'submit001').click()
-        # time.sleep(5)
+        ##Иногда принтер находится в режиме энергосбережения, в этом случае проверяем этот факт, и выводим его из этого режима
+        try: exit_from_standby_mode(driver)
+        except: pass
 
-        frame =  driver.find_element(By.NAME,'wlmframe')
+        login(driver)
+
+        driver.find_element(By.ID, 's81').click()
+        frame = driver.find_element(By.NAME, 'deviceconfig')
         driver.switch_to.frame(frame)
-        driver.find_element(By.ID, 'arg01_UserName').send_keys(printer_info["login"])
-        driver.find_element(By.ID, 'arg02_Password').send_keys(printer_info["password"])
-        driver.find_element(By.NAME, 'Login').click()
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        content_table = soup.find(id='contentrow')
+        content_table_td = soup.find_all('td')
 
-        time.sleep(10)
-
-        print(f'get_counters{printer_info}')
-        print(f'{printer_info["ip_address"]}/')
-        print(url)
+        print('---------------')
+        print(f'Копирование: {content_table_td[15].text}')
+        print('---------------')
+        print(f'Принтер: {content_table_td[22].text}')
+        print('---------------')
+        print(f'Факс: {content_table_td[28].text}')
+        print('---------------')
+        print(f'Общий: {content_table_td[35].text}')
+        print('================')
 
 
